bf37442cc47080c29cae4639ab23f67d712937e3
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-08-29 21:53:42 +0800

    chore: restore to default settings

--- stylesync/settings.py
+++ stylesync/settings.py
@@ -38,9 +38,9 @@ FIREBASE_CONFIG = {
 SECRET_KEY = os.environ.get(
     'SECRET_KEY', default='django-insecure-%9+#8yu!req7ho+p2hjwj!kv%4u8!ssj5a78r@_t70gp7q14d2')
 
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['onlyfrogs-stylesync-ml-server-production.up.railway.app']
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
