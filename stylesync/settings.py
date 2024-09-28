55d8f633d1a90f90fdd351269a78b4d436e75a07
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-09-28 21:43:59 +0800

    fix: add static and collected static files to .gitignore

--- stylesync/settings.py
+++ stylesync/settings.py
@@ -139,12 +139,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Static settings for production
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Serve static files locally in development
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
